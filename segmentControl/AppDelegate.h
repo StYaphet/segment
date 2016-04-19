bf28cf60b7f61b125fac50aaf8081856db2156b8
haoyipeng <haoyipeng@chunyu.me>
2016-04-19 11:39:39 +0800

    Initial Commit

---
+++ segmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  segmentControl
+//
+//  Created by admin on 16/4/19.
+//  Copyright © 2016年 chunyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ segmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  segmentControl
+//
+//  Created by admin on 16/4/19.
+//  Copyright © 2016年 chunyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
